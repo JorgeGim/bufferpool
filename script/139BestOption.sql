4aa3cb1d9aba79d83e9e770dadbfe7486e7c47f3
Jorge Gimenez <jorgegimenez_1996@live.com>
2018-10-10 20:45:26 -0300

    139 Best option

---
+++ script/139BestOption.sql
@@ -0,0 +1,10 @@
+select get_disk_page(20);
+select get_disk_page(21);
+select get_disk_page(22);
+select get_disk_page(23);
+select get_disk_page(24);
+select get_disk_page(20);
+select get_disk_page(25);
+select get_disk_page(21);
+select get_disk_page(20);
+select get_disk_page(21);
